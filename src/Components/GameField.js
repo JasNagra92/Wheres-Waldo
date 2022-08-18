f61ab954be7da9bd8c77d80a15f3ab234d9c169e
newbiejas <jaskarannagra@homail.com>
2022-08-17 21:24:08 -0700

    initialize firestore to retrieve waldo coordinates from backend on component mount as well as firecloud storage to retrieve image to put in playing field

--- src/Components/GameField.js
+++ src/Components/GameField.js
@@ -1,13 +1,40 @@
 import React, { useState, useContext, useEffect } from 'react';
 import styles from '../Styles/GameField.module.css';
 import { TimerContext } from './TimerContext';
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { getFirestore, collection, doc, getDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAs4GeX2Z7al_5yOQKbG4OPi852PsunYBk',
+  authDomain: 'wheres-waldo-66f99.firebaseapp.com',
+  projectId: 'wheres-waldo-66f99',
+  storageBucket: 'wheres-waldo-66f99.appspot.com',
+  messagingSenderId: '328279096571',
+  appId: '1:328279096571:web:ec3985028c9053d8c3445e',
+  measurementId: 'G-78816RL902',
+};
 
 const GameField = () => {
   const [timer, setTimer] = useContext(TimerContext);
   const [seconds, setSeconds] = useState(0);
+  const [timerActive, setTimerActive] = useState(true);
+
   const [coordinates, setCoordinates] = useState({});
-  const [selected, setSelected] = useState('goku');
+  const [selected, setSelected] = useState('waldo');
   const [formVisibility, setFormVisibility] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const storage = getStorage(app);
+  const docRef = doc(db, 'Coordinates', 'Waldo Coordinates');
+
+  const test = async () => {
+    const docSnap = await getDoc(docRef);
+    const waldoCoordinates = docSnap.data()
+    console.log(waldoCoordinates['x-max'])
+  };
 
   const handleClick = (e) => {
     setFormVisibility(!formVisibility);
@@ -15,16 +42,30 @@ const GameField = () => {
       offsetx: e.nativeEvent.offsetX,
       offsety: e.nativeEvent.offsetY,
     });
-    setInterval(() => {
-      setSeconds((seconds) => seconds + 1);
-    }, 1000);
+    if (timerActive) {
+      setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    }
   };
+
   useEffect(() => {
     setTimer(seconds);
   }, [seconds]);
+
   const handleChange = (e) => {
     setSelected(e.target.value);
   };
+
+  useEffect(() => {
+    const pathReference = ref(storage, 'WheresWaldo.jpeg');
+
+    const getImage = async () => {
+      setImageUrl(await getDownloadURL(pathReference));
+    };
+    getImage();
+    test()
+  });
   let viewMode = {};
   let selectBox = {};
 
@@ -32,16 +73,16 @@ const GameField = () => {
     viewMode = {
       display: 'block',
       position: 'absolute',
-      left: coordinates.offsetx,
+      left: coordinates.offsetx + 10,
       top: coordinates.offsety,
     };
     selectBox = {
       display: 'block',
       position: 'absolute',
       top: coordinates.offsety - 15,
-      left: coordinates.offsetx - 25,
+      left: coordinates.offsetx - 15,
       width: '20px',
-      height: '20px',
+      height: '35px',
       border: '1px solid black',
     };
   } else {
@@ -55,16 +96,11 @@ const GameField = () => {
 
   return (
     <div onClick={(e) => handleClick(e)} className={styles.container}>
-      <div>
-        <p>offset x: {coordinates.offsetx}</p>
-        <p>offset y: {coordinates.offsety}</p>
-        <p>clientx: {coordinates.clientx}</p>
-        <p>clienty: {coordinates.clienty}</p>
-      </div>
+      <img src={imageUrl} alt="wheres waldo" />
       <div>
         <form style={viewMode} onSubmit={(e) => e.preventDefault()}>
           <select value={selected} onChange={handleChange}>
-            <option value="goku">Goku</option>
+            <option value="Waldo">Waldo</option>
             <option value="gohan">Gohan</option>
           </select>
         </form>
