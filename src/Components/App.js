93cb378898fdf3b12ff8d5d0c23c6b643099f2c6
newbiejas <jaskarannagra@homail.com>
2022-08-17 19:28:56 -0700

    start field component that will hold image

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import GameField from "./GameField";
+function App() {
+  return (
+    <div className="App">
+      <div>
+
+      </div>
+
+      <div>
+        <GameField />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/GameField.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../Styles/GameField.module.css"
+
+const GameField = () => {
+    const []
+
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+export default GameField
